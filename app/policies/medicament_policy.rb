8d26514ffe9eb03466383a8009affcb35a601539
robson3999 <robertglod104@gmail.com>
2019-08-21 20:14:51 +0200

    Added Pundit and devise-i18n

---
+++ app/policies/medicament_policy.rb
@@ -0,0 +1,2 @@
+class MedicamentPolicy < ApplicationPolicy
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
   resources :medicaments
   resources :medicament_groups
   resources :substances
